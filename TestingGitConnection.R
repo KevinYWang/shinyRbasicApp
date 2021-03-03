ba41c4e289595f1d37e1521dff7188d264f64a94
Kevin <hlcwang@gmail.com>
2021-03-03 23:43:31 +1300

    Hello world Shiny App

--- TestingGitConnection.R
+++ TestingGitConnection.R
@@ -1,2 +1,21 @@
-install.packages('ggplot2')
-library(ggplot2)
+# install.packages('shiny')
+library(shiny)
+
+baseUI <- fluidPage(titlePanel('My Shiny Demo App'),
+                    sidebarLayout(sidebarPanel(
+                      textInput('myNameInput', 'Please input your name here')
+                    ),
+                    mainPanel(
+                      paste('You claim your name is '),
+                      textOutput('myNameOutput')
+                    )))
+
+
+baseServer <- shinyServer(function(input, output) {
+  output$myNameOutput  <- renderText({
+    paste(input$myNameInput)
+  })
+})
+
+
+shinyApp(ui = baseUI, server = baseServer)
